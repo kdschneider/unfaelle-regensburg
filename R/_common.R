102e17ff1905245fb952218c2191cf468ba454fe
Konstantin Schneider <ks@konstantin-schneider.eu>
2021-09-12 02:46:23 +0200

    copy pasta from existing file

---
+++ R/_common.R
@@ -0,0 +1,14 @@
+# R options set globally
+options(width = 60)
+
+# chunk options set globally
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  message = FALSE,
+  warning = FALSE,
+  # figure options
+  fig.align = "center",
+  fig.retina = 3,
+  out.width = "95%"
+)
