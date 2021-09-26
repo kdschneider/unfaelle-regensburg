8b64d173360b0e8f06637eba72d7b9a3086d448b
Konstantin Schneider <ks@konstantin-schneider.eu>
2021-09-26 16:34:25 +0200

    Update _common.R

--- R/_common.R
+++ R/_common.R
@@ -7,8 +7,28 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   message = FALSE,
   warning = FALSE,
+  echo = TRUE,
+  cache = FALSE,
   # figure options
+  fig.width = 6,
+  fig.height = 4,
   fig.align = "center",
   fig.retina = 3,
   out.width = "95%"
 )
+
+# ggplot2
+
+ggplot2::theme_set(
+  ggthemes::theme_clean()
+)
+
+# colour theme 
+
+## fill colour scale
+scale_fill_custom <- 
+  viridis::scale_fill_viridis(
+    discrete = TRUE,
+    option = "D",
+    direction = -1
+  )
