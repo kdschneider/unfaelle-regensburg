657be45a9fd88966a00c11a63542bfc991af41ea
Konstantin Schneider <ks@konstantin-schneider.eu>
2021-09-30 21:42:34 +0200

    added info to dashboard

--- R/_deploy.R
+++ R/_deploy.R
@@ -1,5 +1,4 @@
 # build all
-
 source(
   here::here("R/_build-book.R")
 )
@@ -8,6 +7,8 @@ source(
   here::here("R/_build-assignment.R")
 )
 
-# source(
-#   here::here("R/_publish-dashboard.R")
-# )
+cleanEnv <- new.env()
+source(
+  file = here::here("R/_publish-dashboard.R"),
+  local = cleanEnv
+)
